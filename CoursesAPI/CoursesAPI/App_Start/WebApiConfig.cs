aa9878f2dd82c21a5dbb7ad7e583923c211be322
Eldho <eldho@gmail.com>
2020-01-16 08:44:27 +0530

    first-commit

---
+++ CoursesAPI/CoursesAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace CoursesAPI
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+        
+
+            
+
+            EnableCorsAttribute cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors();
+
+            //var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
+            //jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+        }
+    }
+}
---
+++ CoursesAPI/CoursesAPI/Controllers/CoursController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.Http.Description;
+using CoursesAPI.Models;
+
+namespace CoursesAPI.Controllers
+{
+    [EnableCorsAttribute("*", "*", "*")]
+    public class CoursController : ApiController
+    {
+        private SqlConStr db = new SqlConStr();
+
+        // GET: api/Cours
+        public List<Cours> GetCourses()
+        {
+            List<Cours> ls = new List<Cours>();
+            ls = db.Courses.ToList();
+            return ls;
+        }
+
+        // GET: api/Cours/5
+        [ResponseType(typeof(Cours))]
+        public IHttpActionResult GetCours(long id)
+        {
+            Cours cours = db.Courses.Find(id);
+            if (cours == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cours);
+        }
+
+        // PUT: api/Cours/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutCours(long id, Cours cours)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != cours.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(cours).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CoursExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Cours
+        [ResponseType(typeof(Cours))]
+        public IHttpActionResult PostCours(Cours cours)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Courses.Add(cours);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = cours.Id }, cours);
+        }
+
+        // DELETE: api/Cours/5
+        [ResponseType(typeof(Cours))]
+        public IHttpActionResult DeleteCours(long id)
+        {
+            Cours cours = db.Courses.Find(id);
+            if (cours == null)
+            {
+                return NotFound();
+            }
+
+            db.Courses.Remove(cours);
+            db.SaveChanges();
+
+            return Ok(cours);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CoursExists(long id)
+        {
+            return db.Courses.Count(e => e.Id == id) > 0;
+        }
+    }
+}
